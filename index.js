324342a1958080f0bd9651b509f6703354a290af
Ludovic Mantovani <ludovic.mantov@gmail.com>
2022-09-22 12:00:43 +0200

    Show glob

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const core = require('@actions/core');
+const glob = require('@actions/glob');
 
 async function run() {
   try {
+    core.info(glob)
     core.info('Search for missing package-lock.json files');
   }
   catch (error) {
