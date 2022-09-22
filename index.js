2bce5617dcca9344db8d31f77ae58bc15953aee4
Ludovic Mantovani <ludovic.mantov@gmail.com>
2022-09-22 11:54:07 +0200

    Test warning

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 async function run() {
   try {
-    core.info('Search for missing package-lock.json files');
+    core.warning('Search for missing package-lock.json files');
   }
   catch (error) {
     core.setFailed(error.message);
